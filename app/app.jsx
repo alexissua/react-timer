baf316c956b11f320b64bda400f02cc1561f578a
Alexis Suarez <alexissuarez@Alexiss-MacBook-Pro.local>
2016-12-04 12:50:23 -0500

    New react-timer just with Nav component

---
+++ app/app.jsx
@@ -0,0 +1,26 @@
+// Importamos las librerías::
+var React = require("react");
+var ReactDOM = require("react-dom");
+
+//Route, Router, IndexRoute, hashHistory son variables que acceden a la propiedad de react-router::
+//EJ: var Route = require("react-router").Route;
+var {Route, Router, IndexRoute, hashHistory} = require("react-router");
+var Main = require("./components/Main");
+
+
+// Load Foundation
+require("style!css!foundation-sites/dist/foundation.min.css");
+$(document).foundation();
+
+// Load CSS File::
+require("style!css!sass!applicationStyles");
+
+ReactDOM.render(
+    <Router history={hashHistory} >
+      <Route path="/" component={Main}>
+          
+      </Route>
+
+    </Router>,
+    document.getElementById("app")
+);
